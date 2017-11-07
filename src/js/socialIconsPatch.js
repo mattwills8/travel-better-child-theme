d96d25555284fcb23bd8cae86acce5d1ac09c906
mattwills8 <matt_wills8@hotmail.co.uk>
2017-11-07 13:21:57 -0500

    move title across to be in line with article text

--- src/js/socialIconsPatch.js
+++ src/js/socialIconsPatch.js
@@ -12,13 +12,16 @@
     var containerWidth = $('.container.container-single').width()
 
     var right = ((window.innerWidth - containerWidth)/2) - 20;
+    var headerRight = ((window.innerWidth - 750)/2) - 20;
 
     var paddingRight = 0;
 
-    if( right < socialIconsWidth ) {
+    if( headerRight < socialIconsWidth ) {
 
-      paddingRight = socialIconsWidth - right + 10;
-      $('.header-wrapper').css({'padding-right':paddingRight});
+      paddingRight = socialIconsWidth - headerRight + 10;
+      paddingRightFloor = paddingRight < 0 ? 0 : paddingRight;
+
+      $('.header-standard').css({'padding-right':paddingRightFloor});
     }
 
     socialIcons.each(function() {
