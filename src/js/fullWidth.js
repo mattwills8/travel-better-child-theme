24c252271fb7baf150335cd90103571a9b1a02ca
mattwills8 <matt_wills8@hotmail.co.uk>
2017-11-08 17:03:40 -0500

    refactor width js to allow us to give a width, update footer width

--- src/js/fullWidth.js
+++ src/js/fullWidth.js
@@ -1,49 +1,65 @@
 
 (function($) {
 
-  var selectorsArray = [
-    {
-      parent: '.inner-post-entry',
-      child: '.tb-fullwidth',
-    },
-    {
-      parent: '.single .container-single',
-      child: '.post-image',
-    },
-    {
-      parent: 'article.post',
-      child: '.post-pagination',
-    },
-    {
-      parent: 'article.post',
-      child: '.post-related',
-    },
-  ];
+  function selectorsArray() {
+    return [
+      {
+        parent: '.inner-post-entry',
+        child: '.tb-fullwidth',
+      },
+      {
+        parent: '.single .container-single',
+        child: '.post-image',
+      },
+      {
+        parent: 'article.post',
+        child: '.post-pagination',
+        width: $('#widget-area .container').width(),
+      },
+      {
+        parent: 'article.post',
+        child: '.post-related',
+        width: $('#widget-area .container').width(),
+      },
+    ];
+  }
 
   $(document).ready(function() {
-    makeElementsFullWidth(selectorsArray)
+    setElementWidths(selectorsArray());
   });
 
   $(window).resize(function() {
-    makeElementsFullWidth(selectorsArray)
+    setElementWidths(selectorsArray());
   });
 
 
-  function makeElementsFullWidth( selectorsArray ) {
+  function setElementWidths( selectorsArray ) {
 
     for(i=0; i < selectorsArray.length; i++) {
 
-      setFullWidth( selectorsArray[i].parent, selectorsArray[i].child );
+      if( ! selectorsArray[i].width ) {
+        setFullWidth( selectorsArray[i].parent, selectorsArray[i].child );
+        continue;
+      }
+
+      setWidth( selectorsArray[i].parent, selectorsArray[i].child, selectorsArray[i].width);
     }
   }
 
+
   function setFullWidth( parentFixedWidthSelector, childSelector ) {
+
+    setWidth( parentFixedWidthSelector, childSelector, window.innerWidth );
+  }
+
+
+  function setWidth( parentFixedWidthSelector, childSelector, finalWidth ) {
 
     var parent = $(parentFixedWidthSelector);
 
     parent.each(function() {
 
-      var marginSize = ((window.innerWidth-$(this).width())/2);
+      var marginSize = ((finalWidth-$(this).width())/2);
 
       $(this).find(childSelector).each(function() {
         $(this).css({
